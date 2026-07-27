2eab3e21df6d4f74d177caa01d71518ba9d7b3d5
Jitesh Kumar Viwhal <jiteshkumarviwhal@gmail.com>
2026-07-27 11:58:18 +0530

    Implement path simplification logic

---
+++ Simplify Path.js
@@ -0,0 +1,20 @@
+ let stack = [];
+
+    let parts = path.split('/');
+
+    for (let part of parts) {
+
+        if (part === '' || part === '.') {
+            continue;
+        }
+
+        if (part === '..') {
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else {
+            stack.push(part);
+        }
+    }
+
+    return '/' + stack.join('/');
