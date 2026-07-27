b0a8612e09874194128a0f941bf6e7ddc7ee026a
Jitesh Kumar Viwhal <jiteshkumarviwhal@gmail.com>
2026-07-27 11:52:36 +0530

    Implement parentheses matching function

---
+++ paranthesis.js
@@ -0,0 +1,25 @@
+ let stack = [];
+
+    for (let ch of s) {
+
+        if (ch === '(' || ch === '{' || ch === '[') {
+            stack.push(ch);
+        } else {
+
+            if (stack.length === 0) {
+                return false;
+            }
+
+            let top = stack.pop();
+
+            if (
+                (ch === ')' && top !== '(') ||
+                (ch === '}' && top !== '{') ||
+                (ch === ']' && top !== '[')
+            ) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
